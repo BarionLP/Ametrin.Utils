375e0c71c00a483b878911e8a4fe09226a41778a
Barion <ametrin.studios@gmail.com>
2023-08-07 16:21:11 +0200

    fixes

--- FileSystemInfoExtensions.cs
+++ FileSystemInfoExtensions.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.VisualBasic.FileIO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ametrin.Utils;
 
