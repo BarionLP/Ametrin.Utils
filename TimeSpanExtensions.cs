dbe204725ef5c4a432f449d525e53a2b0e723dbc
Barion <kothe@botos.de>
2023-12-01 15:05:09 +0100

    added timespan extensions

---
+++ TimeSpanExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Ametrin.Utils;
+
+public static class TimeSpanExtensions{
+    public static bool Approximately(this TimeSpan x, TimeSpan y, TimeSpan tolerance) => x.Difference(y) <= tolerance;
+    public static TimeSpan Difference(this TimeSpan x, TimeSpan y) => x > y ? x - y : y - x;
+}
