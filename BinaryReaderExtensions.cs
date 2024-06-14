7efc763ee6be8a1dcff4f88617bc79e946b5cb1c
BarionLP <ametrin.studios@gmail.com>
2024-06-14 11:17:13 +0200

    reworking the binary reader/writer extensions

--- BinaryReaderExtensions.cs
+++ BinaryReaderExtensions.cs
@@ -1,8 +1,6 @@
 ﻿namespace Ametrin.Utils;
 
 public static class BinaryReaderExtensions {
-    
-    #region Big Endian
     public static double ReadDoubleBigEndian(this BinaryReader reader) => reader.ReadBigEndian(BitConverter.ToDouble, 8);
     public static float ReadSingleBigEndian(this BinaryReader reader) => reader.ReadBigEndian(BitConverter.ToSingle, 4);
 
@@ -24,32 +22,6 @@ public static class BinaryReaderExtensions {
             buffer.Reverse();
         }
     }
-    #endregion
-
-
-    #region Little Endian
-    public static double ReadDoubleLittleEndian(this BinaryReader reader) => reader.ReadLittleEndian(BitConverter.ToDouble, 8);
-    public static float ReadSingleLittleEndian(this BinaryReader reader) => reader.ReadLittleEndian(BitConverter.ToSingle, 4);
-
-    public static int ReadInt32LittleEndian(this BinaryReader reader) => reader.ReadLittleEndian(BitConverter.ToInt32, 4);
-    public static uint ReadUInt32LittleEndian(this BinaryReader reader) => reader.ReadLittleEndian(BitConverter.ToUInt32, 4);
-
-    public static short ReadInt16LittleEndian(this BinaryReader reader) => reader.ReadLittleEndian(BitConverter.ToInt16, 2);
-    public static ushort ReadUInt16LittleEndian(this BinaryReader reader) => reader.ReadLittleEndian(BitConverter.ToUInt16, 2);
-
-    public static T ReadLittleEndian<T>(this BinaryReader reader, Converter<T> converter, int byteSize) where T : struct {
-        Span<byte> buffer = stackalloc byte[byteSize];
-        reader.ReadLittleEndian(buffer);
-        return converter(buffer);
-    }
-
-    public static void ReadLittleEndian(this BinaryReader reader, Span<byte> buffer) {
-        reader.Read(buffer);
-        if(!BitConverter.IsLittleEndian) {
-            buffer.Reverse();
-        }
-    }
-    #endregion
+    
+    public delegate T Converter<out T>(ReadOnlySpan<byte> buffer);
 }
-
-public delegate T Converter<out T>(ReadOnlySpan<byte> buffer);
