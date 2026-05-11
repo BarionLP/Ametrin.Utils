a29b833c32bbd23883ac828bccf66e9cc5a5a62d
Barion <barion+git@ik.me>
2026-05-11 12:49:00 +0200

    add NotifyCollectionChangedEventArgs.Replace make NotifyCollectionChangedEventArgs.Reset a lazy singleton

--- src/NotifyCollectionChangedExtensions.cs
+++ src/NotifyCollectionChangedExtensions.cs
@@ -4,21 +4,24 @@ namespace Ametrin.Utils;
 
 public static class NotifyCollectionChangedExtensions
 {
+    public static NotifyCollectionChangedEventArgs ResetArgs => field ??= new(NotifyCollectionChangedAction.Reset);
     extension(NotifyCollectionChangedEventArgs)
     {
-        public static NotifyCollectionChangedEventArgs Add<T>(T item)
+        public static NotifyCollectionChangedEventArgs Add<T>(T item, int index = -1)
         {
-            return new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item);
+            return new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index);
         }
 
-        public static NotifyCollectionChangedEventArgs Remove<T>(T item)
+        public static NotifyCollectionChangedEventArgs Replace<T>(T newItem, T oldItem, int index = -1)
         {
-            return new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item);
+            return new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, newItem, oldItem, index);
         }
 
-        public static NotifyCollectionChangedEventArgs Reset()
+        public static NotifyCollectionChangedEventArgs Remove<T>(T item, int index = -1)
         {
-            return new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+            return new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, index);
         }
+
+        public static NotifyCollectionChangedEventArgs Reset => ResetArgs;
     }
 }
