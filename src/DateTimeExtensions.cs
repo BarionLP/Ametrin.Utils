368cf1202750b49863baca5bc6f08129ec6d8324
Barion <barion+git@ik.me>
2026-05-12 14:08:23 +0200

    add DateTime.GetNewer

--- src/DateTimeExtensions.cs
+++ src/DateTimeExtensions.cs
@@ -7,5 +7,9 @@ public static class DateTimeExtensions
         public static DateTime GetOlder(DateTime x, DateTime y) => x < y ? x : y;
         public static DateTime? GetOlder(DateTime? x, DateTime? y)
             => x.HasValue ? y.HasValue ? GetOlder(x.Value, y.Value) : x : y;
+
+        public static DateTime GetNewer(DateTime x, DateTime y) => x < y ? y : x;
+        public static DateTime? GetNewer(DateTime? x, DateTime? y)
+            => x.HasValue ? y.HasValue ? GetNewer(x.Value, y.Value) : x : y;
     }
 }
--- test/Test_DateTimeExtensions.cs
+++ test/Test_DateTimeExtensions.cs
@@ -3,7 +3,7 @@ namespace Ametrin.Utils.Test;
 public sealed class Test_DateTimeExtensions
 {
     [Test]
-    public async Task Test()
+    public async Task GetOlder()
     {
         var min = DateTime.MinValue;
         var max = DateTime.MaxValue;
@@ -16,5 +16,21 @@ public sealed class Test_DateTimeExtensions
         await Assert.That(DateTime.GetOlder(null, now)).IsEqualTo(now);
         await Assert.That(DateTime.GetOlder(null, null)).IsNull();
         await Assert.That(DateTime.GetOlder(new DateTime?(min), new DateTime?(now))).IsEqualTo(min);
+    }
+
+    [Test]
+    public async Task GetNewer()
+    {
+        var min = DateTime.MinValue;
+        var max = DateTime.MaxValue;
+        var now = DateTime.Now;
+
+        await Assert.That(DateTime.GetNewer(min, max)).IsEqualTo(max);
+        await Assert.That(DateTime.GetNewer(now, max)).IsEqualTo(max);
+        await Assert.That(DateTime.GetNewer(now, now)).IsEqualTo(now);
+        await Assert.That(DateTime.GetNewer(now, null)).IsEqualTo(now);
+        await Assert.That(DateTime.GetNewer(null, now)).IsEqualTo(now);
+        await Assert.That(DateTime.GetNewer(null, null)).IsNull();
+        await Assert.That(DateTime.GetNewer(new DateTime?(min), new DateTime?(now))).IsEqualTo(now);
     }
 }
